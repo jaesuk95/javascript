6c93050b893f4831ee89e8f1d136de2ade0c43a0
jaesuk95 <jaesuk95@outlook.com>
2022-03-20 03:59:58 +0900

    Second Commit

--- js-session15-moralis-server/dex.js
+++ js-session15-moralis-server/dex.js
@@ -14,7 +14,8 @@ Moralis.start({ serverUrl, appId });
 Moralis.initPlugins().then(() => console.log("plugins have been initialised"));  // buy crypto
 
 const $tokenBalanceTBody = document.querySelector('.js-token-balances');
-
+const $selectedToken = document.querySelector('.js-from-token');    // refer as selected token
+const $amountInput = document.querySelector('.js-from-amount');
 
 //Convert token value to ETH style with 18 decimals
 //If you do not specify decimals, 18 decimals will be automatically used
@@ -35,22 +36,54 @@ async function login() {
     getStats();
 }
 
+// swap function
+async function initSwapForm(event){
+    event.preventDefault();             // preventDefault() method cancels the event if it is cancelable, ability to prevent a browser’s default behavior for events.
+    // // e.target은 사용자가 클릭한 <li> 요소를 가리킴
+    $selectedToken.innerText = event.target.dataset.symbol;     // target is the button which we clicked on, and the button has data (symbol)
+    // we will then need to send address and decimals once we submit swap itself
+    // target은 이벤트가 일어날 객체를 의미한다. 예를 들어 버튼을 누르면 새로운 창이 열리는 객체가 있다고 하자. 그렇다면 여기서 버튼은 event target이 된다.
+    // e.target 을 통해 현재 이벤트 일어난 요소를 가져오고, 그 요소의 data-address 로 저장한 데이터 값은 data 속성에 접근하는 속성인 'dataset' 과 data-뒤에 저장한 이름을 매치시켜부른다 
+    $selectedToken.dataset.address = event.target.dataset.address;
+    $selectedToken.dataset.decimals = event.target.dataset.decimals;
+    $selectedToken.dataset.max = event.target.dataset.max;
+    $amountInput.removeAttribute('disabled');   // this enabled input values 
+    $amountInput.value =''; // clear the value because it can happen it is already enabled 
+    document.querySelector('.js-submit').removeAttribute('disabled');
+    document.querySelector('.js-cancel').removeAttribute('disabled');   
+    document.querySelector('.js-quote-container').innerHTML = '';           // generate a result automatically 
+}
+
 // because we want to use await function that is why we are using async function
-async function getStats(){
-    const balances = await Moralis.Web3API.account.getTokenBalances({chain: 'polygon'});    // chain = the blockchain to get data from
-    console.log(balances);
+async function getStats() {
+    const balances = await Moralis.Web3API.account.getTokenBalances({ chain: 'polygon' });    // chain = the blockchain to get data from
+    console.log(balances);  // provides what coins you hold
     $tokenBalanceTBody.innerHTML = balances.map((token, index) => `
         <tr>
             <td>${index + 1}</td>
             <td>${token.symbol}</td>
             <td>${tokenValue(token.balance, token.decimals)}</td>
-            <td>button</td>
+            <td>
+                <button class="js-swap"
+                    data-address="${token.token_address}"
+                    data-symbol="${token.symbol}"
+                    data-decimals="${token.decimals}"
+                    data-max="${tokenValue(token.balance, token.decimals)}">
+                    Swap
+                </button>
+            </td>
         </tr>
     `).join('');
+
+    // to make sure all the clicks are listened, create a for loop
+    // for each buttons(class named .js-swap) that have tokenBalanceTBody
+    for(let $btn of $tokenBalanceTBody.querySelectorAll('.js-swap')){
+        $btn.addEventListener('click', initSwapForm);
+    }
 }
 
 // buying crypto function by using moralis -> onRamp (https://moralis.io/plugins/fiat/)
-async function buyCrypto(){
+async function buyCrypto() {
     Moralis.Plugins.fiat.buy();
 }
 
